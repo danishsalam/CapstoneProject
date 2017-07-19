ced7caa4f6f4f84091145e62fe29254e95211806
Salam <m.danish.salam@gmail.com>
2017-07-18 20:09:16 -0500

    Add Featurization

--- featurization.R
+++ featurization.R
@@ -1,11 +1,27 @@
+library(dplyr)
+library(tidyr)
 raw_ss <- read.csv("stdscore_raw.csv")
-df <- tbl_df(rafw_ss)
+df <- tbl_df(raw_ss) 
 
 df %>% 
-  filter(SubjectIdentifier=="1110") %>% 
-  group_by(SubjectIdentifier) %>% 
-  summarise(stdCount = n()) %>% 
-  summarise(avg = mean(stdCount))
+  group_by(gradingPeriod, AYIdentifier, School_Domain, SubjectIdentifier) %>% 
+  summarise(AssignCount = n_distinct(AssignmentName)) %>% 
+  ungroup() %>%
+  summarise(xmin = min(AssignCount), xmax = max(AssignCount), xmedain = median(AssignCount), xmean = mean(AssignCount))
+  
+
+df %>% 
+  group_by(gradingPeriod, AYIdentifier, School_Domain, SubjectIdentifier) %>% 
+  summarise(ScoreCount = n(), amax = max(Score)) 
+
+
+%>% 
+  ungroup() %>%
+  select (ScoreCount, amin, amax, xmean, xmedeian)
+
+
+gp_AssignCount %>% 
+  summarise(xmin = min(AssignCount), xmax = max(AssignCount), xmedain = median(AssignCount), xmean = mean(AssignCount))
 
 df %>% 
   filter(SubjectIdentifier=="1110") %>% 
